fe60dfa4f84b9d8737901e155c450224bfe19864
jimmihugh <jimmihugh@hotmail.co.uk>
2018-04-17 16:18:38 +0100

    Basic environment setup

--- heap.c
+++ heap.c
@@ -2,8 +2,12 @@
 
 void* malloc (size_t size)
 {
+	return (void*)size;
 }
 
-void free (void* ptr);
+void free (void* ptr)
 {
+	int* intptr = (int*)ptr;
+	*intptr = 0x00000000;
+	return;
 }
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "heap.h"
 
+int kmain (void);
+
 int kmain (void)
 {
 	return 0;
